f936677a8498e4748bc412b860e64c630d353fb7
Arjp80 <ajjj47726@gmail.com>
2025-06-06 02:47:55 +0330

    completing FrameHandler header file

--- libs/FrameHandler/FrameHandler.h
+++ libs/FrameHandler/FrameHandler.h
@@ -5,11 +5,33 @@
 #ifndef FRAMEHANDLER_H
 #define FRAMEHANDLER_H
 
+// header file is written by alireza jahani 
 
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 class FrameHandler {
+public:
+ 
+    FrameHandler();
+    
+    
+    ~FrameHandler();
+    
+    cv::Mat getFrame();
+    
+    void adjustFrameSize(cv::Mat& frame);
 
+private:
+    cv::VideoCapture cap; 
+    int current_width;    
+    int current_height;   
+    
+    cv::Rect detectSubject(cv::Mat& frame);
+    
+    cv::Size calculateNewSize(cv::Rect subject_area);
 };
+
 
 
 
